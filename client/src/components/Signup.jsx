6de7e9fa86b59b5e777504a17f6ddb7da0db5218
codewithumar <codewithumar@gmail.com>
2023-08-18 23:21:07 +0500

    Client::Auth integrated in frontend

---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Signup = () => {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:3001/auth/signup", {
+        name: name,
+        phone: phone,
+        email: email,
+        password: password,
+        role: "user",
+      });
+      console.log(res.data.auth);
+      if (res.data.auth) {
+        localStorage.setItem("token", res.data.token);
+        navigate(`/home/${res.data.addeddata.userid}`);
+      }
+    } catch (error) {
+      setError("Pelase provide correct data");
+      console.error("Login failed:", error);
+    }
+  };
+
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6 col-sm-8">
+          <h2 className="text-center mb-4">Sign Up</h2>
+          {error && <p className="text-danger">{error}</p>}
+          <form onSubmit={handleSignUp}>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
+                Name:
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="phone" className="form-label">
+                Phone:
+              </label>
+              <input
+                type="tel"
+                className="form-control"
+                id="phone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email:
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password:
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div className="text-center">
+              <button type="submit" className="btn btn-primary">
+                Sign Up
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
